94327d579a4ddd5fd457ae9eb28f21b16852fcdf
Miguel <m.a.corleon1@gmail.com>
2024-02-26 19:27:57 +0100

    Avance de la práctica de JS

--- FormularioConJs/validador_enunciat.js
+++ FormularioConJs/validador_enunciat.js
@@ -1,5 +1,24 @@
 const form = document.querySelector('form');
-const nom = document.querySelector("nom");
+const nom = document.querySelector('#nom');
+const llin1 = document.querySelector('#llin1');
+const llin2 = document.querySelector('#llin2');
+const pais = document.querySelector('#pais');
+const captcha = document.querySelector('#label-captcha');
+const usuari = document.querySelector('#usuari');
+const password = document.querySelector('#password');
+const dataNaix = document.querySelector('#dataNaix');
+const dni = document.querySelector('dni');
+/* Attach events oninput*/
+
+nom.oninput = () => { validaNom(24, 2, nom, "nombre"); }
+llin1.oninput = () => { validaNom(24, 2, llin1, "primer apellido"); }
+llin2.oninput = () => { validaNom(24, 2, llin2, "segundo apellido"); }
+usuari.oninput = () => { validaUsuari(20, 4, usuari, "Usuario"); }
+password.oninput = () => { validapassword(16, 8, password, "contraseña"); }
+captcha.onsubmit = () => { validaCaptcha(); }
+pais.oninput = () => { validaPais(); }
+dataNaix.oninput = () => { validaEdad(dataNaix); }
+dni.oninput = () => { validadni(); }
 
 const usuaris = ["daniel.82", "manuel-223", "darknight_1", "mrNum.1234", "user77", "fairyGoth"];
 
@@ -15,7 +34,7 @@ function setValidity(element, msgError) {
     }
 
     element.setCustomValidity(msgError);
-    document.querySelector(`#error-${element.id}`).textContent = msgError;
+    document.querySelector(`#error-${element.id}`).innerHTML = msgError;
 }
 
 form.onsubmit = function (event) {
@@ -34,35 +53,137 @@ form.onsubmit = function (event) {
     form.classList.add('was-validated');
 };
 
-form.onreset = () =>{}
+form.onreset = () => { }
 
-/* Attach events oninput*/
-nom.oninput = validaNom;
+
 
 
 /*funcion de validación*/
-function validaNom(){
-   if(nomvalue.length > 24){
-     msgError = "No tiene que ser superior a 24 carácteres";
-   }
-   
-};
+function validaNom(max, min, texto, campo) {
+    let msgError = "";
+    const longitud = texto.value.length;
+    if (longitud > max || longitud < min) {
+        msgError = `El ${campo} tiene que tener entre ${min} y ${max} caracteres `;
+    }
 
-function soloLetras(texto){
-   var valida = /^[A-Za-zÁ-]+$/.test(texto);
-   if (valida){
-    return "";
-   }
+    msgError += soloLetras(texto);
+    setValidity(texto, msgError);
 
-   return "Solo se pueden letras"
 }
 
-setValidity(nom,msgError);
+function validaUsuari(max, min, texto, campo) {
+    let msgError = "";
+    const longitud = texto.value.length;
+    if (longitud > max || longitud < min) {
+        msgError = `El ${campo} tiene que tener entre ${min} y ${max} caracteres.`;
+    }
+
+    msgError += verificaUsuario(texto);
+    setValidity(texto, msgError);
+}
+
+function validapassword(max, min, texto, campo) {
+    let msgError = "";
+    const longitud = texto.value.length;
+    if (longitud > max || longitud < min) {
+        msgError = `El ${campo} tiene que tener entre ${min} y ${max} caracteres `;
+    }
+
+    msgError += verificadoPassword(texto);
+    setValidity(texto, msgError);
+
+}
+
+function verificaUsuario(texto) {
+    let valida = /^[a-zA-Z][a-zA-Z0-9]*(?:[-_\.][a-zA-Z0-9]*)?$/g.test(texto.value);
+    if (!valida) {
+        return " Debe empezar con una letra y puede contener un solo simbolo(-, _, .) `";
+    } else {
+        return "";
+    }
+}
+
+
+function soloLetras(texto) {
+    let valida = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s]+$/g.test(texto.value);
+    if (!valida) {
+        return "No puede contener números";
+    } else {
+        return "";
+    }
+}
+
+function verificadoPassword(texto) {
+    let valida = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-_.$#])[\s\S]{8,}$/g.test(texto.value);
+    if (!valida) {
+        return " Debe contener mínimo una mayúscula, minúscula,número y símbolo  (-, _, ., $, #)";
+    } else {
+        return "";
+    }
+}
+
+function validaEdad(fecha) {
+    let msgError = "";
+    let edadCliente = fecha.value;
+    let fechaActual = new Date();
+    edadCliente = new Date(edadCliente);
+    let mayorDeEdad = fechaActual.getFullYear() - edadCliente.getFullYear();
+    let mesNacimiento = fechaActual.getMonth() - edadCliente.getMonth();
+    if (mesNacimiento > 0 || (mesNacimiento == 0 && fechaActual.getDate() < edadCliente.getDate())) {
+        --mayorDeEdad;
+    }
+
+    if (mayorDeEdad < 18) {
+        msgError += "Es menor de edad";
+    } else msgError += "";
+    setValidity(fecha, msgError);
+
+}
+
+function validaPais() {
+    if (pais.value !== "ES") {
+        cp.disabled = true;
+        dni.disabled = true;
+        cp.value = "";
+        dni.value = "";
+    } else {
+        cp.disabled = false;
+        dni.disabled = false;
+    }
+}
+function validadni() {
+    msgError = "";
+    let valida = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
+    if (!valida) {
+        return "No es un DNI valido";
+    } else {
+        return "";
+    }
+
+}
+
+
+
 /* CAPTCHA */
-var generaCaptcha = function(){
 
-}();
 
-function validaCaptcha(){
+function generaCaptcha() {
+    let num1 = Math.round(Math.random() * 10);
+    let num2 = Math.round(Math.random() * 10);
+    let num3 = Math.round(Math.random());
+    let operacio = num3 ? "-" : "+";
 
+    if (num1 > num2) {
+        captcha.textContent = `${num1}${operacio}${num2}`;
+    } else {
+        captcha.textContent = `${num2}${operacio}${num1}`;
+    }
+};
+generaCaptcha();
+
+function validaCaptcha() {
+    if (generaCaptcha.value == eval(document.querySelector("#label_captcha").textContent)) setValidity(captcha, "correcto");
+    else {
+        setValidity(captcha, "error");
+    }
 }
