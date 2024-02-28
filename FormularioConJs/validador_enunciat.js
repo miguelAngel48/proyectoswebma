affc63465ce7df628a794158f450fccc4a959911
Miguel <m.a.corleon1@gmail.com>
2024-02-28 23:56:59 +0100

    Prueba desde el  ordenador

--- FormularioConJs/validador_enunciat.js
+++ FormularioConJs/validador_enunciat.js
@@ -4,10 +4,15 @@ const llin1 = document.querySelector('#llin1');
 const llin2 = document.querySelector('#llin2');
 const pais = document.querySelector('#pais');
 const captcha = document.querySelector('#label-captcha');
+const regenera = document.querySelector('#regenera');
 const usuari = document.querySelector('#usuari');
 const password = document.querySelector('#password');
 const dataNaix = document.querySelector('#dataNaix');
-const dni = document.querySelector('dni');
+const dni = document.querySelector('#dni');
+const cp = document.querySelector('#cp');
+const listaNombres = document.querySelector('#listaNombres');
+const eliminar = document.querySelector('#eliminar');
+
 /* Attach events oninput*/
 
 nom.oninput = () => { validaNom(24, 2, nom, "nombre"); }
@@ -16,11 +21,14 @@ llin2.oninput = () => { validaNom(24, 2, llin2, "segundo apellido"); }
 usuari.oninput = () => { validaUsuari(20, 4, usuari, "Usuario"); }
 password.oninput = () => { validapassword(16, 8, password, "contraseña"); }
 captcha.onsubmit = () => { validaCaptcha(); }
+regenera.onclick = () => { generaCaptcha(); }
 pais.oninput = () => { validaPais(); }
 dataNaix.oninput = () => { validaEdad(dataNaix); }
-dni.oninput = () => { validadni(); }
+dni.oninput = () => { validadni(dni); }
+cp.oninput = () => { validaCP(cp); }
 
-const usuaris = ["daniel.82", "manuel-223", "darknight_1", "mrNum.1234", "user77", "fairyGoth"];
+
+
 
 /* Funció que marca els inputs com a vàlids/invàlids*/
 function setValidity(element, msgError) {
@@ -51,9 +59,16 @@ form.onsubmit = function (event) {
     }
 
     form.classList.add('was-validated');
+
 };
 
-form.onreset = () => { }
+form.onreset = () => {
+    form.querySelectorAll("input").forEach(function (element) {
+        element.classList.remove("is-invalid", "is-valid");
+
+    });
+
+}
 
 
 
@@ -142,25 +157,47 @@ function validaEdad(fecha) {
 
 function validaPais() {
     if (pais.value !== "ES") {
-        cp.disabled = true;
-        dni.disabled = true;
-        cp.value = "";
-        dni.value = "";
+        disabledPais();
     } else {
         cp.disabled = false;
         dni.disabled = false;
     }
 }
-function validadni() {
-    msgError = "";
-    let valida = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
-    if (!valida) {
-        return "No es un DNI valido";
-    } else {
-        return "";
-    }
-
+function disabledPais() {
+    cp.disabled = true;
+    dni.disabled = true;
+    cp.value = "";
+    dni.value = "";
+    cp.classList.remove("is-invalid");
+    cp.classList.remove("is-valid");
+    dni.classList.remove("is-invalid");
+    dni.classList.remove("is-valid");
 }
+function validadni(texto) {
+    let msgError = "";
+    let valida = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i.test(texto.value);
+    if (!valida) {
+        msgError += "No es un DNI válido";
+    } else {
+        msgError += "";
+
+    }
+    setValidity(texto, msgError);
+}
+
+function validaCP(texto) {
+    msgError = "";
+    let valida = /^[0-9]{5}$/i.test(texto.value);
+    if (!valida) {
+        msgError += "No es un Código Postal válido";
+    } else {
+        msgError += "";
+
+    }
+    setValidity(texto, msgError);
+}
+
+/* funciones para Localstorage */
 
 
 
@@ -178,12 +215,116 @@ function generaCaptcha() {
     } else {
         captcha.textContent = `${num2}${operacio}${num1}`;
     }
-};
+}
 generaCaptcha();
 
 function validaCaptcha() {
-    if (generaCaptcha.value == eval(document.querySelector("#label_captcha").textContent)) setValidity(captcha, "correcto");
-    else {
+    let respuesta = document.querySelector("#captcha").value;
+    let respuestaCorrecta = eval(document.querySelector("#label-captcha").textContent);
+
+    if (respuesta == respuestaCorrecta) {
+        setValidity(captcha, "correcto");
+    } else {
         setValidity(captcha, "error");
     }
 }
+
+/*
+function AlmacenarDatos() {
+
+    let objetosRecuperados = obteArraySessionStorage("datosElegidos");
+
+    const nuevoObjeto = creaObjeto();
+
+    let encontrado = false;
+    objetosRecuperados.forEach(element => {
+        if (element.usuari == nuevoObjeto.usuari) {
+            encontrado = true;
+            return;
+        }
+    });
+
+    if (encontrado) {
+        alert("Este usuario ya existe!");
+    } else {
+        objetosRecuperados.push(nuevoObjeto);
+        sessionStorage.setItem("datosElegidos", JSON.stringify(objetosRecuperados));
+
+        actualizaSelect(objetosRecuperados);
+
+        this.reset();
+    }
+
+}
+
+function actualizaSelect(objetos) {
+    listaNombres.innerHTML = "";
+
+    let resultado = "";
+    for (const objeto of objetos) {
+        resultado += `<option value="${objeto.usuari}">${objeto.nom} (Edad: ${objeto.edadCliente})</option>`;
+    }
+
+    listaNombres.innerHTML = resultado;
+}
+
+function creaObjeto() {
+    return {
+        nombre: nom.value,
+        primerApellido: llin1.value,
+        segundoApellido: llin2.value,
+        usuari: usuari.value,
+        Pais: pais.value,
+        password: password.value,
+        dataNaix: dataNaix.value,
+        dni: dni.value,
+        cp: cp.value,
+    }
+}
+
+listaNombres.onchange = function () {
+
+    const datosElegidos = listaNombres.value;
+
+    let objetosRecuperados = obteArraySessionStorage("datosElegidos");
+
+    let objetoEncontrado = null;
+    for (const objeto of objetosRecuperados) {
+        if (objeto.usuari == datosElegidos) {
+            objetoEncontrado = objeto;
+            break;
+        }
+    }
+
+    if (objetoEncontrado) {
+        nom.value = objetoEncontrado.nom;
+        llin1.value = objetoEncontrado.llin1;
+        llin2.value = objetoEncontrado.llin2;
+        usuari.value = objetoEncontrado.usuari;
+        pais.value = objetoEncontrado.pais;
+        password.value = objetoEncontrado.password;
+        dataNaix.value = objetoEncontrado.dataNaix;
+        dni.value = objetoEncontrado.dni;
+        cp.value = objetoEncontrado.cp;
+    }
+}
+
+eliminar.onclick = () => {
+    sessionStorage.clear();
+    actualizaSelect([]);
+}
+
+document.body.onload = () => {
+    actualizaSelect(obteArraySessionStorage("datosElegidos"));
+}
+
+function obteArraySessionStorage(param) {
+    let objetosRecuperados = [];
+    let listaNombres = sessionStorage.getItem(param);
+
+    if (listaNombres != null) {
+        objetosRecuperados = JSON.parse(listaNombres);
+    }
+
+    return objetosRecuperados;
+} */
